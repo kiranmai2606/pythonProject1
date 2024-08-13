cb010ca15cee329c833692522f7af7393f8d7080
kiranmai2606 <50226540+kiranmai2606@users.noreply.github.com>
2024-08-13 12:11:12 +0530

    Tasks on Arithmatic operations & Math Table

---
+++ Tasks130824/Lab002.py
@@ -0,0 +1,17 @@
+"""Task #2
+
+Create a program , take 2 inputs from the user num1, num2 and give them
+max
+pow num1 to num2
+sub, mul, sum, div.
+Format your out with f{""}"""
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+print("Maximum of two numbers:",max(num1,num2))
+print("power num1 to num2 :",pow(num1,num2))
+print("Sum of two numbers:",(num1+num2))
+print("Differance of two numbers:",(num1-num2))
+print("Multiplication of two numbers:",(num1*num2))
+print("Division of two numbers:",(num1/num2))
+
+
---
+++ Tasks130824/Lab003.py
@@ -0,0 +1,10 @@
+"""Can you create a Program I will give you number(userinput and print table)
+f"{}" String format concept
+User input - num int -> 10, 100, -1, 2, 3.14 -> input
+9x1 = 9
+9x2 = 18... till 10
+"""
+num =int(input("Enter a number:"))
+for i in range(1,11):
+    print(num,"*", (i),"=" ,num*i)
+
